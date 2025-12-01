d1b1dcdbeda97df2a89699313a01fda3599eb334
Biwbong <bewrockgame1@gmail.com>
2025-12-01 13:16:40 +0700

    เปลี่ยนใช้ url ในไฟล์ prisma.config.ts

--- Backend/src/prisma.ts
+++ Backend/src/prisma.ts
@@ -1,6 +1,7 @@
 // src/prisma.ts
 
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 
-export default prisma;
+export const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});

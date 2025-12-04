d2cb9bf84372cca9f693c70777d4f76da5da98e0
BiwBong1706 <bewrockgame1@gmail.com>
2025-12-05 03:47:40 +0700

    Update CustomerInfoTable.tsx

--- Frontend/smartdorm-Detail/src/components/Booking/CustomerInfoTable.tsx
+++ Frontend/smartdorm-Detail/src/components/Booking/CustomerInfoTable.tsx
@@ -1,27 +1,22 @@
+// Booking/components/Booking/CustomerInfoTable.tsx
 import type { Booking, Customer } from "../../types/booking";
 import CustomerInfoRow from "./CustomerInfoRow";
 
-export default function CustomerInfoTable({
-  booking,
-  customer,
-}: {
-  booking: Booking;
-  customer: Customer;
-}) {
+export default function CustomerInfoTable({ booking, customer }: { booking: Booking; customer: Customer }) {
   return (
-    <div className="mb-3">
+    <div className="mb-4">
       <h5 className="fw-bold text-primary text-center mb-3">ข้อมูลผู้จอง</h5>
-      <table className="table table-sm table-bordered">
-        <thead>
+      <table className="table table-bordered table-sm text-center align-middle shadow-sm">
+        <thead className="table-light">
           <tr>
-            <th className="text-center">รายการ</th>
-            <th className="text-center">ข้อมูล</th>
+            <th>รายการ</th>
+            <th>ข้อมูล</th>
           </tr>
         </thead>
         <tbody>
           <CustomerInfoRow label="ห้อง" value={booking.room.number} />
-          <CustomerInfoRow label="Line" value={customer.userName} />
-          <CustomerInfoRow label="ชื่อผู้จอง" value={booking.fullName} />
+          <CustomerInfoRow label="LINE ผู้ใช้" value={customer.userName} />
+          <CustomerInfoRow label="ชื่อ-สกุล" value={booking.fullName} />
           <CustomerInfoRow label="เบอร์โทร" value={booking.cphone} />
         </tbody>
       </table>

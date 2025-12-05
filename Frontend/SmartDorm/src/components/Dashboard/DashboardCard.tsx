317de3bd07211fc260728d616f13ddfb642ccc06
BiwBong1706 <bewrockgame1@gmail.com>
2025-12-05 18:44:55 +0700

    Update DashboardCard.tsx

--- Frontend/SmartDorm/src/components/Dashboard/DashboardCard.tsx
+++ Frontend/SmartDorm/src/components/Dashboard/DashboardCard.tsx
@@ -13,10 +13,10 @@ export default function DashboardCard({ title, count, link }: DashboardCardProps
     <div
       className="card text-center border-0 shadow-sm"
       style={{
-        background: "linear-gradient(135deg, #4A0080, #2A0052)", // SCB Purple
-        color: "#F7D53D", // SCB Gold
+        background: "linear-gradient(135deg, #4A0080, #2A0052)", // SCB purple
+        color: "#F7D53D", // SCB gold
         borderRadius: "18px",
-        height: "90px",
+        height: "95px",
         cursor: "pointer",
         transition: "transform .15s",
       }}
@@ -25,10 +25,10 @@ export default function DashboardCard({ title, count, link }: DashboardCardProps
       onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1.0)")}
     >
       <div className="d-flex flex-column justify-content-center align-items-center h-100">
-        <div className="fw-bold" style={{ fontSize: "1.15rem" }}>
+        <div className="fw-bold" style={{ fontSize: "1.1rem" }}>
           {title}
         </div>
-        <div className="fw-bold" style={{ fontSize: "1.4rem" }}>
+        <div className="fw-bold" style={{ fontSize: "1.6rem" }}>
           {count}
         </div>
       </div>

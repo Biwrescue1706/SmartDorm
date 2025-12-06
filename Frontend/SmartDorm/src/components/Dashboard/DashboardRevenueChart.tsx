9dd4dad2c3d8510d150a358ae8d524e8d472427e
BiwBong1706 <bewrockgame1@gmail.com>
2025-12-07 04:41:37 +0700

    Update DashboardRevenueChart.tsx

--- Frontend/SmartDorm/src/components/Dashboard/DashboardRevenueChart.tsx
+++ Frontend/SmartDorm/src/components/Dashboard/DashboardRevenueChart.tsx
@@ -12,14 +12,16 @@ import {
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Legend, Tooltip);
 
 export default function DashboardRevenueChart({ labels, datasets, title }: any) {
-  const data = { labels, datasets };
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: { position: "top" },
-      title: { display: true, text: title },
-    },
-  };
-
-  return <Line data={data} options={options} />;
+  return (
+    <Line
+      data={{ labels, datasets }}
+      options={{
+        responsive: true,
+        plugins: {
+          legend: { position: "top" as const },
+          title: { display: true, text: title },
+        },
+      }}
+    />
+  );
 }

0ca804022c1f16f601fcd6f1658f392950fee42c
BiwBong1706 <bewrockgame1@gmail.com>
2026-01-02 23:41:42 +0700

    Update BillDetail.tsx

--- Frontend/smartdorm-Detail/src/pages/BillDetail.tsx
+++ Frontend/smartdorm-Detail/src/pages/BillDetail.tsx
@@ -124,8 +124,8 @@ export default function BillDetail() {
           {/* BILL INFO */}
           <div className="bg-light p-3 rounded border mb-3 mt-2">
             <p className="mb-1">
-              <strong>Line ผู้เช่า  :</strong> {booking.customer?.userName ?? "-"}
-            </p>
+  <strong>Line ผู้เช่า  :</strong> {bill.customer?.userName ?? "-"}
+</p>
             <p className="mb-1">
               <strong>ผู้เช่า :</strong> {fullName}
             </p>

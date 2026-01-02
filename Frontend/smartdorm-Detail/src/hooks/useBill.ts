210e6629d558e3a82ba6c6e8ce2070914797f2c5
BiwBong1706 <bewrockgame1@gmail.com>
2026-01-02 23:59:19 +0700

    Update useBill.ts

--- Frontend/smartdorm-Detail/src/hooks/useBill.ts
+++ Frontend/smartdorm-Detail/src/hooks/useBill.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Bill } from "../types/bill";
+import type { Bill } from "../types/bill";
 import { API_BASE } from "../config";
 
 export function useBill(billId?: string) {

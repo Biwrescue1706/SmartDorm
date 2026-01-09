e673437e39fb7c9c54eda516a52018086fe9dd0e
BiwBong1706 <bewrockgame1@gmail.com>
2026-01-09 18:13:50 +0700

    Update AllBillsRow.tsx

--- Frontend/SmartDorm/src/components/AllBills/AllBillsRow.tsx
+++ Frontend/SmartDorm/src/components/AllBills/AllBillsRow.tsx
@@ -5,7 +5,7 @@ interface Props {
   bill: Bill;
   role?: number | null;
   onEdit: (bill: Bill) => void;
-  onDelete: (billId: string) => void;
+  onDelete: (billId: string, room?: string) => void; // ✅ เพิ่ม room?
   onViewSlip: (bill: Bill) => void;
   onManage: (bill: Bill) => void;
 }

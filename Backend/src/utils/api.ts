78a2479fb19b062aa62e3c1cfafb466fd28422b4
BiwBong1706 <bewrockgame1@gmail.com>
2026-01-21 13:28:28 +0700

    Update api.ts

--- Backend/src/utils/api.ts
+++ Backend/src/utils/api.ts
@@ -1,2 +1,2 @@
-const BASE_URL = "https://details.smartdorm-biwboong.shop";
-const ADMIN_URL = "https://manage.smartdorm-biwboong.shop";
+export const BASE_URL = "https://details.smartdorm-biwboong.shop";
+export const ADMIN_URL = "https://manage.smartdorm-biwboong.shop";

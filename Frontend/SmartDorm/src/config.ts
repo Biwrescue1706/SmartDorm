4f0c53ee619d7b5c8bc44c65f07597f738cb2428
BiwBong1706 <bewrockgame1@gmail.com>
2026-01-21 12:19:58 +0700

    Update config.ts

--- Frontend/SmartDorm/src/config.ts
+++ Frontend/SmartDorm/src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE = "https://smartdorm-backend.biwbong.shop";
+export const API_BASE = "https://hub.smartdorm-biwboong.shop";
 //export const API_BASE = "http://localhost:10000";

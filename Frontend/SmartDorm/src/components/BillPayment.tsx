15bbe60bfa7b79bb86108ff258bcda98116699d4
BiwBong1706 <bewrockgame1@gmail.com>
2026-02-16 00:37:20 +0700

    Update BillPayment.tsx

--- Frontend/SmartDorm/src/components/BillPayment.tsx
+++ Frontend/SmartDorm/src/components/BillPayment.tsx
@@ -1,11 +1,11 @@
 import type { Bill } from "../types/All";
-import { formatThai } from "../utils/billUtils";
 
-interface Props {
+export type Props = {
   bill: Bill;
-}
+  formatThai: (d: string) => string;
+};
 
-export default function BillPayment({ bill }: Props) {
+export default function BillPayment({ bill, formatThai }: Props) {
   if (bill.billStatus !== 1 || !bill.paidAt) return null;
 
   return (
@@ -31,58 +31,6 @@ export default function BillPayment({ bill }: Props) {
           <div className="d-flex justify-content-between">
             <span>วันที่ชำระ :</span>
             <span>{formatThai(bill.paidAt)}</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="row mt-4 text-center">
-        {/* ผู้รับ */}
-        <div className="col">
-          <div className="fw-bold">ผู้รับ</div>
-
-          <div>
-            <img
-              src="/assets/signature.webp"
-              alt="ลายเซ็นต์"
-              width={150}
-              height={75}
-            />
-          </div>
-
-          <div>( นายภูวณัฐ พาหะละ )</div>
-
-          <div className="text-muted">
-            {formatThai(bill.paidAt)}
-          </div>
-        </div>
-
-        {/* ผู้จ่าย */}
-        <div className="col">
-          <div className="fw-bold">ผู้จ่าย</div>
-
-          {bill.fullName === "นายภูวณัฐ พาหะละ" ? (
-            <img
-              src="/assets/signature.webp"
-              alt="ลายเซ็นต์"
-              width={150}
-              height={75}
-            />
-          ) : (
-            <>
-              <br />
-              <div>
-                {bill.cname || bill.csurname
-                  ? `${bill.cname ?? ""} ${bill.csurname ?? ""}`
-                  : "-"}
-              </div>
-              <br />
-            </>
-          )}
-
-          <div>({bill.fullName})</div>
-
-          <div className="text-muted">
-            {formatThai(bill.paidAt)}
           </div>
         </div>
       </div>

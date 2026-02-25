e14704daace6b4f01398d0207e6de59e7bdcb091
BiwBong1706 <bewrockgame1@gmail.com>
2026-02-25 13:47:08 +0700

    Update EditRoomDialog.tsx

--- Frontend/SmartDorm/src/components/Room/EditRoomDialog.tsx
+++ Frontend/SmartDorm/src/components/Room/EditRoomDialog.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 import { useRooms } from "../../hooks/ManageRooms/useRooms";
-import type { Room } from "../../types/Room";
+import type { Room } from "../../types/All";
 import { createPortal } from "react-dom";
 
 interface Props {

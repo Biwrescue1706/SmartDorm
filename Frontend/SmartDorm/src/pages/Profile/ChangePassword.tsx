1eb32d909230b39e4d7dc45a1da1ef4edcf15e63
BiwBong1706 <bewrockgame1@gmail.com>
2026-03-10 15:50:35 +0700

    Update ChangePassword.tsx

--- Frontend/SmartDorm/src/pages/Profile/ChangePassword.tsx
+++ Frontend/SmartDorm/src/pages/Profile/ChangePassword.tsx
@@ -80,7 +80,10 @@ export default function ChangePassword() {
 
         <main
           className="main-content flex-grow-1 px-2 py-3 mt-6 mt-lg-7"
-          style={{ paddingLeft: "20px", paddingRight: "20px" }}
+          style={{ 
+paddingLeft: "20px", 
+paddingRight: "20px" 
+}}
         >
           <div className="mx-auto" style={{ maxWidth: "1400px" }}>
             {/* Responsive container */}
